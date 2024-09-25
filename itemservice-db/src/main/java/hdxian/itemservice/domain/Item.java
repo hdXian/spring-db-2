7daba0df313dac1a0546f49a9d9a914c2dcaa675
hdXian <jason123325@gmail.com>
2024-09-25 17:04:29 +0900

    #7 2 - apply JPA on domain, repository, config

--- itemservice-db/src/main/java/hdxian/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hdxian/itemservice/domain/Item.java
@@ -1,16 +1,24 @@
 package hdxian.itemservice.domain;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "item") // able to omit (if same)
 public class Item {
 
+    // @Id means PK. // @GeneratedValue: Key generation strategy is Identity. (DB auto generate)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Mapping property name to DB table Column. it's able to omit if both is same.
+    @Column(name = "item_name", length = 10) // length can be used in DDL (create table ...)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    // public or protected basic constructor needed. (JPA spec)
     public Item() {
     }
 
